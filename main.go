036744d85e3c8bbb04b5afef2917b29eb23e070a
Satyaprakash Singh Yadav <yaduvanshisatya07@gmail.com>
2024-11-27 11:45:51 +0530

    formHandler
    Writing Form Handler logic

--- main.go
+++ main.go
@@ -1,24 +1,35 @@
 package main
-import(
+
+import (
 	"fmt"
 	"log"
 	"net/http"
 )
-func helloHandler( w http.ResponseWriter , r *http.Request)
 
-func formHandler( w http.ResponseWriter, r *http.Request)
-
-
-func main(){
-fileServer := http.FileServer(http.Dir("./static"))//it will look for static file and by default pick the index.html
-http.Handle("/", fileServer) //handle root route which is just above 
-http.HandleFunc("/form", formHandler) // handle formhandler
-http.HandleFunc("/hello", helloHandler) //handle hellohandler
-
-fmt.Printf("Starting server at port 8080 \n") 
-//starting server on 8080 
-if err:= http.ListenAndServer(":8080", nil); err != nil{
-	log.fatal(err)
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello!!!!!")
 }
+
+func formHandler(w http.ResponseWriter, r *http.Request)
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static")) //it will look for static file and by default pick the index.html
+	http.Handle("/", fileServer)                        //handle root route which is just above
+	http.HandleFunc("/form", formHandler)               // handle formhandler
+	http.HandleFunc("/hello", helloHandler)             //handle hellohandler
+
+	fmt.Printf("Starting server at port 8080 \n")
+	//starting server on 8080
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 
 }
